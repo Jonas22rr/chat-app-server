b3da9c34a42061bc067ba4da2f80f0da5cf0b0b5
Jonas <jonasbott2@web.de>
2024-06-02 21:05:20 +0200

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getChat(): string {
-    return this.appService.getChat();
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
