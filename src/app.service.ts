67057182e20cc9ddaf6568d440f851dbf0dab542
Jonas <jonasbott2@web.de>
2024-06-02 21:02:16 +0200

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getChat(): string {
+    return 'New Chat App!';
   }
 }
